46160be4fbb7dab70f3af8fde2495bd4770ed998
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-02-03 11:21:08 +0530

    backend: initial commit

---
+++ TechJob-SpringBoot/src/main/java/com/tech_job/TechJob/TechJobApplication.java
@@ -0,0 +1,13 @@
+package com.tech_job.TechJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechJobApplication.class, args);
+	}
+
+}
---
+++ TechJob-SpringBoot/src/test/java/com/tech_job/TechJob/TechJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_job.TechJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
